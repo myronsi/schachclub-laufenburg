78ac068771c6ba5cd812a151eebb117d82bdce61
jkegithub <bernd77@gmx.de>
2025-03-22 23:22:36 +0100

    KI-Bilder fuer Ostern + vom Wasserschloß in Newslist

--- src/components/arrays/newsList.ts
+++ src/components/arrays/newsList.ts
@@ -3,18 +3,18 @@ export const news = [
       id: 1,
       title: "Osterdienstag",
       description: "Am Dienstag, 22. April, findet kein Spieleabend statt.",
-      image: "/imgs/schach_bunt.png",
+      image: "/imgs/Der_16x9_KI_Osterhase_spielt_Schach.jpg",
     },
     {
       id: 2,
       title: "Neue HomePage !!",
-      description: "NACH 9 JAHREN haben wir endlich einen frischen modernen pflegeleichten Java-Code unter Homepage",
-      image: "/imgs/robot_spielt_schach.webp",
+      description: "NACH 9 JAHREN haben wir endlich einen frischen modernen pflegeleichten Java-Code unter der Homepage",
+      image: "/imgs/robot_spielt_schach_16x9.webp",
     },
-    // {
-    //   id: 3,
-    //   title: "...",
-    //   description: "...",
-    //   image: "https://images.unsplash.com/photo-1580541832626-2a7131ee809f",
-    // },
+    {
+      id: 3,
+      title: "Wasserschloß Inzlingen",
+      description: "Ergebnisse vom Internationalen Schachturnier Anfang März mit 3 Teilnehmern vom SC-Laufenburg (Benno Moser, Jürgen Keßler, Lukas Wegner",
+      image: "/imgs/Wasserschloss_Inzlingen.jpg",
+    },
   ];
