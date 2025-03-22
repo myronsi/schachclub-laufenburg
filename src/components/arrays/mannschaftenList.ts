756af3393e1d00efeadd82ec39f3c571090b6596
jkegithub <bernd77@gmx.de>
2025-03-22 23:38:03 +0100

    WasserschlossTurnierSeite

--- src/components/arrays/mannschaftenList.ts
+++ src/components/arrays/mannschaftenList.ts
@@ -6,4 +6,12 @@ export const teams = [
       image: "imgs/photo_mannschaften1.png",
       url: "https://bsv-ergebnisdienst.de/index.php?p1=0:rm:BK9-24"
     },
+    {
+      id: 2,
+      name: "13. Wasserschloss – OPEN – Inzlingen vom 07. – 09.03.2025",
+      league: "Veranstalter Schachverein Brombach",
+      image: "imgs/Wasserschloss_Inzlingen.jpg",
+      url: "https://schachclub-brombach.de/verein-2/wasserschloss-open-inzlingen/13-wasserschloss-open-inzlingen-vom-07-07-03-2025/"
+    },
+
   ];
