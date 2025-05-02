2d01ee3f96e032cd7944cfd48eba8ae021a7af57
jkegithub <bernd77@gmx.de>
2025-05-03 01:52:46 +0200

    imgs Pfade zu app/imgs geändert ( Empfehlung KI wegen Trennung old HP)

--- src/components/arrays/youthList.ts
+++ src/components/arrays/youthList.ts
@@ -1,22 +1,22 @@
 export const imageUrls = [
     {
-      src: "imgs/jugendspiele.jpg",
+      src: "app/imgs/jugendspiele.jpg",
       alt: "Laufenburger Kindersommer 30.07.12 - 01.08.12"
     },
     {
-      src: "imgs/jugendspiele1.jpg",
+      src: "app/imgs/jugendspiele1.jpg",
     },
     {
-      src: "imgs/jugendspiele2.jpg"
+      src: "app/imgs/jugendspiele2.jpg"
     },
     {
-      src: "imgs/jugendspiele3.jpg"
+      src: "app/imgs/jugendspiele3.jpg"
     },
     {
-      src: "imgs/jugendspiele4.jpg"
+      src: "app/imgs/jugendspiele4.jpg"
     },
     {
-      src: "imgs/jugendspiele5.jpg"
+      src: "app/imgs/jugendspiele5.jpg"
     },
   
   ];
